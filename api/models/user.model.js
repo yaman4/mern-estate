d5b614aedfacb00d4a8be3bb151eee889ee7642d
yaman4 <lambayaman4@gmail.com>
2024-05-10 14:07:09 +0530

    signup api route creation

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,12 +4,12 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     password: {
         type: String,
