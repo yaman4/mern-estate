721a9966304dc3f672b9e0acc1c98045c8e710c8
yaman4 <lambayaman4@gmail.com>
2024-05-10 22:15:40 +0530

    updated profile in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import {FaSearch} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const {currentUser} = useSelector(state=> state.user);
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-slate-700'>House</span>
-                    <span className='text-slate-700'>Huntr</span>
+                    <span className='text-slate-700'>Sheltr</span>
                 </h1>
             </Link>
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
@@ -23,10 +24,12 @@ export default function Header() {
                 <Link to='about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                 </Link>
-                <Link to='sign-in'>
-                    <li className=' text-slate-700 hover:underline'>Sign In</li>
+                <Link to='/profile'>
+                    {currentUser ? (
+                    <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+                    ) : <li className=' text-slate-700 hover:underline'>Sign In</li>
+                }
                 </Link>
-                
             </ul>
         </div>
     </header>
