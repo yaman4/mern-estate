994734468fc02ae04efc71554629fa1fba7f0f72
yaman4 <lambayaman4@gmail.com>
2024-05-12 22:47:42 +0530

    sign out functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -66,3 +66,12 @@ export const google = async (req, res, next)=> {
         next(error);
     }
 };
+
+export const signOut = async (req, res, next) => {
+ try {
+    res.clearCookie('access_token');
+    res.status(200).json('User Logged Out');
+ } catch (error) {
+    next(error);
+ }
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { google, signup } from '../controller/auth.controller.js';
+import { google, signOut, signup } from '../controller/auth.controller.js';
 import { signin } from '../controller/auth.controller.js';
 
 
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post('/google', google);
+router.get('/signout', signOut);
 
 export default router;
