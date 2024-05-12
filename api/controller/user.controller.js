7befe5fe2057e5fb92aae3e789d921b204762caa
yaman4 <lambayaman4@gmail.com>
2024-05-12 22:23:27 +0530

    delete user functionality

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -30,3 +30,14 @@ export const updateUser = async (req, res, next)=> {
         next(error);
     }
 };
+
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "Cannot Delete"));
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie('access_token');
+        res.status(200).json('User Deleted');
+    } catch (error) {
+        next(error);
+    }
+};
