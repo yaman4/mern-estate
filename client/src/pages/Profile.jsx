8bcec24d8b7895e1723decbdef5f2c19e38d5d56
yaman4 <lambayaman4@gmail.com>
2024-05-15 20:01:15 +0530

    delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -122,6 +122,25 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -197,7 +216,8 @@ export default function Profile() {
             </Link>
             <div className="flex flex-col items-center">
               <button className="text-red-700
-              uppercase">
+              uppercase"
+              onClick={()=>handleListingDelete(listing._id)}>
                 Delete
               </button>
               <button className="text-green-700
